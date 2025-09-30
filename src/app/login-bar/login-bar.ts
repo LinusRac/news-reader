eea5a35d25bd796e575b7edf6aa3cb791da27e2f
Linus Rachinger <linus.rachinger@fau.de>
2025-09-30 14:09:16 +0200

    added improved placeholder for login bar

--- src/app/login-bar/login-bar.ts
+++ src/app/login-bar/login-bar.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login-bar',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-bar.html',
-  styleUrl: './login-bar.css'
+  styleUrls: ['./login-bar.css']
 })
-export class LoginBar {
+export class LoginBar implements OnInit {
+  username: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onLogin(): void {
+    if (this.username && this.password) {
+      console.log('Logging in with', this.username, this.password);
+      // TODO: Replace with your authentication logic
+      alert(`Welcome, ${this.username}!`);
+    } else {
+      alert('Please enter both username and password.');
+    }
+  }
 }
